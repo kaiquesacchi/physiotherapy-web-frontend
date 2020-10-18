950c4e38b3a93748029bb5d09defd97c18f7f40d
Kaique Sacchi <sacchikaique@gmail.com>
2020-10-18 17:27:13 -0300

    Added default theme and font

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./pages";
 
+import { ThemeProvider } from "styled-components";
+import theme from "./themes";
+
 import { SnackBar } from "./components/SnackBar";
 
 ReactDOM.render(
-  <SnackBar>
-    <App />
-  </SnackBar>,
+  <ThemeProvider theme={theme}>
+    <SnackBar>
+      <App />
+    </SnackBar>
+  </ThemeProvider>,
   document.getElementById("root")
 );
---
+++ src/themes.ts
@@ -0,0 +1,6 @@
+export default {
+    background: "#FBF4ED",
+    primary: "#92CBA9",
+    secondary: "#695849",
+    lightGray: "#909090",
+};
